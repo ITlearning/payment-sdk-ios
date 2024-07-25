18d8ccd1ce175402f38cf62d91ec4b825131aa96
ITlearning <yo7504@naver.com>
2024-07-25 09:55:52 +0900

    * fix: 예외 수정

--- TossPayments/Sources/Util/URLComponent+TossPaymentExtension.swift
+++ TossPayments/Sources/Util/URLComponent+TossPaymentExtension.swift
@@ -25,7 +25,7 @@ extension String {
         //
         // 하지만 "/" 문자를 서버에서 받지 못하는 문제로 인해 포함시킵니다.
         // https://tossteam.slack.com/archives/C7V49P7A9/p1661826985586599
-        let generalDelimitersToEncode = ":#[]@/\""
+        let generalDelimitersToEncode = ":#[]@/"
         let subDelimitersToEncode = "!$&'()*+,;="
 
         var allowedCharacterSet = CharacterSet.urlQueryAllowed
